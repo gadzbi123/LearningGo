66eeb62599d21250d5e45913d5518b274d795eac
Gadzbi <crg1234@gmail.com>
2024-05-30 11:16:33 +0200

    fixed flow of the program

--- led_clock/main.go
+++ led_clock/main.go
@@ -10,131 +10,115 @@ import (
 	_ "github.com/inancgumus/screen"
 )
 
-const zero string = `
- ### 
-#   #
-#   #
-#   #
- ### 
-`
-const one string = `
-#    
-#    
-#    
-#    
-#    
-`
-const two string = `
-#####
-    #
-#####
-#    
-#####
-`
-const three string = `
-#####
-    #
-#####
-    #
-#####
-`
-
-const four string = `
-#   #
-#   #
-#####
-    #
-    #
-`
-
-const five string = `
-#####
-#    
-#####
-    #
-#####
-`
-
-const six string = `
-#####
-#    
-#####
-#   #
-#####
-`
-
-const seven string = `
-#####
-    #
-    #
-    #
-    #
-`
-
-const eight string = `
-#####
-#   #
-#####
-#   #
-#####
-`
-
-const nine string = `
-#####
-#   #
-#####
-    #
-#####
-`
-const middle string = `
-     
-  #  
-     
-  #  
-     
-`
-
-var DIGITS = [10]string{zero, one, two, three, four, five, six, seven, eight, nine}
-
-func AssertEqual(statement bool, format string, args ...interface{}) {
-	if !statement {
-		message := fmt.Sprintf(format+"\n", args...)
-		panic(message)
-	}
+var zero = []string{
+	" ### ",
+	"#   #",
+	"#   #",
+	"#   #",
+	" ### ",
 }
-func getSplittedDigit(digit string) []string {
-	spl_digit := strings.Split(digit, "\n")
-	return spl_digit[1 : len(spl_digit)-1]
+var one = []string{
+	"#    ",
+	"#    ",
+	"#    ",
+	"#    ",
+	"#    ",
 }
+
+var two = []string{
+	"#####",
+	"    #",
+	"#####",
+	"#    ",
+	"#####",
+}
+
+var three = []string{
+	"#####",
+	"    #",
+	"#####",
+	"    #",
+	"#####",
+}
+
+var four = []string{
+	"#   #",
+	"#   #",
+	"#####",
+	"    #",
+	"    #",
+}
+
+var five = []string{
+	"#####",
+	"#    ",
+	"#####",
+	"    #",
+	"#####",
+}
+
+var six = []string{
+	"#####",
+	"#    ",
+	"#####",
+	"#   #",
+	"#####",
+}
+
+var seven = []string{
+	"#####",
+	"    #",
+	"    #",
+	"    #",
+	"    #",
+}
+
+var eight = []string{
+	"#####",
+	"#   #",
+	"#####",
+	"#   #",
+	"#####",
+}
+
+var nine = []string{
+	"#####",
+	"#   #",
+	"#####",
+	"    #",
+	"#####",
+}
+
+var middle = []string{
+	"     ",
+	"  #  ",
+	"     ",
+	"  #  ",
+	"     ",
+}
+
+var DIGITS = [10][]string{zero, one, two, three, four, five, six, seven, eight, nine}
+
 func printDigits(timeClock *TimeClock) {
 	screen.Clear()
 	screen.MoveTopLeft()
-	arr := make([][]string, 5)
-	for i := range arr {
-		arr[i] = make([]string, 0, 6)
-	}
-	var my_screen string = "\033[31m"
-	var spl_mid []string
-	if timeClock.getSplitter() {
-		spl_mid = getSplittedDigit(middle)
-	} else {
-		for i := 0; i < 5; i++ {
-			spl_mid = append(spl_mid, strings.Repeat(" ", 5))
+	var splitter []string = make([]string, 5)
+	copy(splitter, middle)
+	if !timeClock.getSplitter() {
+		for i, v := range splitter {
+			splitter[i] = strings.Replace(v, "#", " ", -1)
 		}
 	}
 
-	for i := 0; i < 5; i++ {
-		for _, timeDigit := range timeClock.getCurrentTime() {
-			splitted := getSplittedDigit(DIGITS[timeDigit])
-			arr[i] = append(arr[i], splitted[i])
-		}
-
-		for j := 0; j < 6; j++ {
-			digit_row := arr[i][j]
+	my_screen := "\033[31m"
+	currTime := timeClock.getCurrentTime()
+	for row := range DIGITS[0] {
+		for digit_pos := range currTime {
+			digit_num := currTime[digit_pos]
+			digit_row := DIGITS[digit_num][row]
 			my_screen += fmt.Sprintf("%v ", digit_row)
-			AssertEqual(len(digit_row) == 5, "Lenght of row should be 5")
-			if j == 1 || j == 3 {
-				my_screen += fmt.Sprintf("%v ", spl_mid[i])
+			if digit_pos == 1 || digit_pos == 3 {
+				my_screen += fmt.Sprintf("%v ", splitter[row])
 			}
 		}
 		my_screen += "\n"
@@ -150,22 +134,25 @@ type TimeClock struct {
 	mu               sync.Mutex
 }
 
-func (tc *TimeClock) updateTime(updater chan interface{}) {
-	var timeArr []int
-	t := time.Now()
-	second := tc.getTimeElement(5)
-	if second != t.Second()%10 {
-		timeArr = make([]int, 6)
-		timeArr[0] = t.Hour() / 10
-		timeArr[1] = t.Hour() % 10
-		timeArr[2] = t.Minute() / 10
-		timeArr[3] = t.Minute() % 10
-		timeArr[4] = t.Second() / 10
-		timeArr[5] = t.Second() % 10
-		tc.mu.Lock()
-		tc.time = timeArr[:]
-		tc.mu.Unlock()
-		updater <- new(interface{})
+func (tc *TimeClock) tryUpdateTime(updater chan interface{}) {
+	ticker := time.NewTicker(time.Millisecond)
+	for range ticker.C {
+		var timeArr []int
+		t := time.Now()
+		second := tc.getTimeElement(5)
+		if currSec := t.Second() % 10; currSec != second {
+			timeArr = make([]int, 6)
+			timeArr[0] = t.Hour() / 10
+			timeArr[1] = t.Hour() % 10
+			timeArr[2] = t.Minute() / 10
+			timeArr[3] = t.Minute() % 10
+			timeArr[4] = t.Second() / 10
+			timeArr[5] = currSec
+			tc.mu.Lock()
+			tc.time = timeArr[:]
+			tc.mu.Unlock()
+			updater <- new(interface{})
+		}
 	}
 }
 
@@ -196,7 +183,7 @@ func (tc *TimeClock) getSplitter() (splitter bool) {
 	return
 }
 
-func updateSplitter(updater chan interface{}) {
+func (tc *TimeClock) tickSplitter(updater chan interface{}) {
 	ticker := time.NewTicker(time.Millisecond * 500)
 	var signal interface{}
 	for range ticker.C {
@@ -207,15 +194,10 @@ func updateSplitter(updater chan interface{}) {
 
 func main() {
 	updater := make(chan interface{})
-	go updateSplitter(updater)
+	go currentTime.tickSplitter(updater)
+	go currentTime.tryUpdateTime(updater)
 	for {
-		select {
-		case <-updater:
-			printDigits(currentTime)
-		default:
-			go currentTime.updateTime(updater)
-			time.Sleep(time.Millisecond)
-		}
+		<-updater
+		printDigits(currentTime)
 	}
-
 }
